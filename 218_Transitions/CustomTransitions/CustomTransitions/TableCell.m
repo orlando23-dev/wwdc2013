5a3b9496ef29dcf2e47f34126719fedc633eb9a0
llv22 <xiandao.airs@gmail.com>
2013-06-30 23:18:03 +0800

    llv22 - selection model of table cell

--- 218_Transitions/CustomTransitions/CustomTransitions/TableCell.m
+++ 218_Transitions/CustomTransitions/CustomTransitions/TableCell.m
@@ -8,6 +8,8 @@
 
 #import "TableCell.h"
 
+const float iSelectDelay = 500;
+
 @implementation TableCell
 
 //- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
@@ -24,6 +26,23 @@
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
+}
+
+/**
+ * see http://stackoverflow.com/questions/11920156/custom-uitableviewcell-selection-style
+ **/
+- (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated
+{
+    if (highlighted) {
+        self.textLabel.textColor = [UIColor whiteColor];
+    } else {
+        // desc - postpone for displaying - http://stackoverflow.com/questions/4007023/blocks-instead-of-performselectorwithobjectafterdelay
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, NSEC_PER_MSEC * iSelectDelay),
+                       dispatch_get_main_queue(),
+                       ^void (void){
+                           self.textLabel.textColor = [UIColor blackColor];
+                       });
+    }
 }
 
 @end
