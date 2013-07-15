6a56d39c37667f417fdb92c2c758bddfc51ef585
llv22 <xiandao.airs@gmail.com>
2013-07-15 17:18:45 +0800

    llv22 - main page setting, flipping effect will be added later

--- 204_Multitasking/MultiTaskforSina/MultiTaskforSina/MasterViewController.h
+++ 204_Multitasking/MultiTaskforSina/MultiTaskforSina/MasterViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MasterViewController : UITableViewController
+@interface MasterViewController : UITableViewController{
+}
 
 @end
--- 204_Multitasking/MultiTaskforSina/MultiTaskforSina/MasterViewController.m
+++ 204_Multitasking/MultiTaskforSina/MultiTaskforSina/MasterViewController.m
@@ -12,6 +12,8 @@
 
 @interface MasterViewController () {
     NSMutableArray *_objects;
+    //desc - NO, by default
+    BOOL _isWeiboSettingPage;
 }
 @end
 
@@ -26,10 +28,6 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
 }
 
 - (void)didReceiveMemoryWarning
