eaba185ae42341e1c0efe032ec29b9c0dfcc4e45
Orlando Ding <xiandao.airs@gmail.com>
2014-02-26 22:59:25 +0800

    llv22 - ITA for thread group synchronization with expensive cost

--- ITA/DynamicOCThread/DynamicOCThread/main.m
+++ ITA/DynamicOCThread/DynamicOCThread/main.m
@@ -17,7 +17,7 @@ static NSMutableDictionary* fibSafeDict = nil;
 
 long Fib (int n){
     if (n <= 1) {
-        return n;
+        return 1;
     }
     else{
 //        long x=Fib(n-1), y=Fib(n-2);
@@ -34,7 +34,7 @@ long Fib (int n){
 
 long FibInParallel (int n){
     if (n <= 1) {
-        return n;
+        return 1;
     }
     else{
         //        long x=Fib(n-1), y=Fib(n-2);
@@ -45,8 +45,8 @@ long FibInParallel (int n){
         if (!fibSafeDict[@(n-1)]) {
             dispatch_async(dispatch_get_main_queue(), ^{
                 _tmpx = FibInParallel(n-1);
+                [fibSafeDict safeSetObject:@(_tmpx) forKey:@(n-1)];
             });
-            [fibSafeDict safeSetObject:@(_tmpx) forKey:@(n-1)];
         }
         else{
             _tmpx = [[fibSafeDict safeObjectForKey:@(n-1)] longValue];
@@ -54,12 +54,54 @@ long FibInParallel (int n){
         if (!fibSafeDict[@(n-2)]) {
             dispatch_async(dispatch_get_main_queue(), ^{
                 _tmpy = FibInParallel(n-2);
+                [fibSafeDict safeSetObject:@(_tmpy) forKey:@(n-2)];
             });
-            [fibSafeDict safeSetObject:@(_tmpy) forKey:@(n-2)];
         }
         else{
             _tmpy = [[fibSafeDict safeObjectForKey:@(n-2)] longValue];
         }
+        return _tmpx + _tmpy;
+    }
+}
+
+long FibInParallelGroup (int n){
+    if (n <= 1) {
+        return 1;
+    }
+    else{
+        //        long x=Fib(n-1), y=Fib(n-2);
+        //        return x+y;
+        dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+        dispatch_group_t group = dispatch_group_create();
+        
+        // TODO : add a task to the group
+        __block long _tmpx, _tmpy;
+        if (!fibSafeDict[@(n-1)]) {
+            dispatch_group_async(group, queue, ^{
+                _tmpx = FibInParallelGroup(n-1);
+                [fibSafeDict safeSetObject:@(_tmpx) forKey:@(n-1)];
+            });
+        }
+        else{
+            _tmpx = [[fibSafeDict safeObjectForKey:@(n-1)] longValue];
+        }
+        if (!fibSafeDict[@(n-2)]) {
+            dispatch_group_async(group, queue, ^{
+                _tmpy = FibInParallelGroup(n-2);
+                [fibSafeDict safeSetObject:@(_tmpy) forKey:@(n-2)];
+            });
+        }
+        else{
+            _tmpy = [[fibSafeDict safeObjectForKey:@(n-2)] longValue];
+        }
+        
+        //TODO : await the group to block the current thread
+        dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+/*  
+        'release' is unavailable: not available in automatic reference counting mode
+        dispatch_release(group);
+ */
+ 
         return _tmpx + _tmpy;
     }
 }
@@ -90,7 +132,7 @@ void SequentialInSingleThread(void)
         [fibDict removeAllObjects];
         
         @autoreleasepool {
-            NSLog(@"%lu", Fib(37400)); // 40000 seems overflow
+            NSLog(@"%lu", Fib(11)); // 40000 seems overflow
         }
     });
     
@@ -107,11 +149,28 @@ void ParallelInMultiThreads(void)
         [fibSafeDict removeAllObjects];
         
         @autoreleasepool {
-            NSLog(@"%lu", FibInParallel(37400)); // 40000 seems overflow
+            NSLog(@"%lu", FibInParallel(11)); // 40000 seems overflow
         }
     });
     
     NSLog(@"Parallelization Fib : %llu ns", n);
+}
+
+void ParallelInMultiThreadsGroup(void)
+{
+    if (!fibSafeDict) {
+        fibSafeDict = [[NSMutableDictionary alloc]init];
+    }
+    
+    uint64_t n = dispatch_benchmark(1/*10, 100, 500, 1000, not cacluated*/, ^{
+        [fibSafeDict removeAllObjects];
+        
+        @autoreleasepool {
+            NSLog(@"%lu", FibInParallelGroup(11)); // 40000 seems overflow
+        }
+    });
+    
+    NSLog(@"ParallelInMultiThreadsGroup Fib : %llu ns", n);
 }
 
 int main(int argc, const char * argv[])
@@ -130,6 +189,8 @@ int main(int argc, const char * argv[])
          2,  - mac pro
          */
         ParallelInMultiThreads();
+        
+        ParallelInMultiThreadsGroup();
         
     }
     return 0;
